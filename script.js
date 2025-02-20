3180446ef74dfa457fa00d8beac1e4d53db8fc85
mkpage1337 <mrku1337@gmail.com>
2025-02-20 13:37:32 +0100

    pokémon-app

---
+++ script.js
@@ -0,0 +1,16 @@
+const userInput = document.getElementById("search-input");
+const submitBtn = document.getElementById("search-button");
+const pokemonImage = document.getElementById("pokemon-image");
+
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonTypes = document.getElementById("types");
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
