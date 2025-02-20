71f6fcc91e2164a4fbf879f9b793accdd43d6aee
mkpage1337 <mrku1337@gmail.com>
2025-02-20 13:43:57 +0100

    pokémon-app

--- script.js
+++ script.js
@@ -14,3 +14,51 @@ const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+
+const searchPokedex = async () => {
+  if (userInput.value === "") {
+    return;
+  }
+
+  try {
+    const res = await fetch(
+      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${userInput.value.toLowerCase()}`
+    );
+    const data = await res.json();
+    console.log(data);
+    const { name, id, weight, height, types, stats, sprites } = data;
+
+    pokemonImage.innerHTML = `
+      <img src="${sprites.front_default}" id="sprite">
+    `;
+
+    pokemonName.innerHTML = name.toUpperCase();
+    pokemonId.innerHTML = `#${id}`;
+
+    pokemonWeight.innerHTML = `Weight: ${weight}`;
+    pokemonHeight.innerHTML = `Height: ${height}`;
+    pokemonTypes.innerHTML = types
+      .map(
+        (type) =>
+          `<span class="${type.type.name.toLowerCase()}">${type.type.name.toUpperCase()}</span>`
+      )
+      .join(" ");
+
+    hp.innerHTML = stats[0].base_stat;
+    attack.innerHTML = stats[1].base_stat;
+    defense.innerHTML = stats[2].base_stat;
+    specialAttack.innerHTML = stats[3].base_stat;
+    specialDefense.innerHTML = stats[4].base_stat;
+    speed.innerHTML = stats[5].base_stat;
+  } catch (err) {
+    console.log(err);
+    alert("Pokemon not found");
+  }
+};
+
+submitBtn.addEventListener("click", searchPokedex);
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchPokedex();
+  }
+});
